c53ee9e9d566e35cb538c3cc1b889160781d8a0e
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-04-25 07:49:19 -0500

    add progressbar , finish code

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ function fetchPokemon(id) {
     .then((res) => res.json())
     .then((data) => {
         createPokemon(data);
+        console.log(data);
         spinner.style.display = "none";
     });
 }
@@ -69,11 +70,47 @@ function createPokemon(pokemon) {
 
     const cardBack = document.createElement('div');
     cardBack.classList.add('pokemon-block-back');
-    cardBack.textContent = "Carta de atrás";
+    
+    cardBack.appendChild(progressBar(pokemon.stats));
 
     cardContainer.appendChild(card);
     cardContainer.appendChild(cardBack);
     pokemonContainer.appendChild(flipCard);
+}
+
+function progressBar(stats) {
+    const statsContainer = document.createElement('div');
+    statsContainer.classList.add("stats-container");
+
+    for (let i = 0; i < 3; i++) {
+         const stat = stats[i];
+
+         const statPercent = stat.base_stat / 2 + "%";
+         const statContainer = document.createElement('div');
+         statContainer.classList.add("stat-container");
+
+         const statName = document.createElement("div");
+         statName.textContent = stat.stat.name;
+
+         const progress = document.createElement("div");
+         progress.classList.add("progress");
+
+         const progressBar = document.createElement("div");
+         progressBar.classList.add("progress-bar");
+         progressBar.setAttribute("aria-valuenow", stat.base_stat);
+         progressBar.setAttribute("aria-valuemin", 0);
+         progressBar.setAttribute("aria-valuemax", 200);
+         progressBar.style.width = statPercent;
+
+         progressBar.textContent = stat.base_stat;
+
+         progress.appendChild(progressBar);
+         statContainer.appendChild(statName);
+         statContainer.appendChild(progress);
+         statsContainer.appendChild(statContainer);
+    }
+
+    return statsContainer
 }
 
 function removeChildNodes(parent) {
