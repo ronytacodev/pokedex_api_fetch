265caf80785037fa921ad86ef8e167cfd8247375
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-04-25 07:18:25 -0500

    add spinner and pagination

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
 const pokemonContainer = document.querySelector(".pokemon-container");
+const spinner = document.querySelector("#spinner");
+const previous = document.querySelector("#previous");
+const next = document.querySelector("#next");
+
+let offset = 1;
+let limit = 8;
+
+previous.addEventListener('click', () => {
+    if (offset != 1) {
+        offset -= 9;
+        removeChildNodes(pokemonContainer);
+        fetchPokemons(offset, limit);
+    }
+})
+
+next.addEventListener('click', () => {
+    offset += 9;
+    removeChildNodes(pokemonContainer);
+    fetchPokemons(offset, limit);
+})
 
 function fetchPokemon(id) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     .then((res) => res.json())
     .then((data) => {
         createPokemon(data);
+        spinner.style.display = "none";
     });
 }
 
-function fetchPokemons(number) {
-    for (let i=1; i <= number; i++) {
+function fetchPokemons(offset, limit) {
+    spinner.style.display = "block";
+    for (let i=offset; i <= offset + limit; i++) {
         fetchPokemon(i);
     }
 }
@@ -40,4 +62,10 @@ function createPokemon(pokemon) {
     pokemonContainer.appendChild(card);
 }
 
-fetchPokemons(9);
+function removeChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
+
+fetchPokemons(offset, limit);
